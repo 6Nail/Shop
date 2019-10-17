069b07a61847d32f946d06284b9cdd144ee4e8de
Наиль Акчурин <golumvicerlid35@gmail.com>
2019-10-17 20:31:42 +0600

    ShopContext

--- Shop/Shop.UI/Program.cs
+++ Shop/Shop.UI/Program.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Shop.DataAccess;
-using Shop.DataAccess.Abstract;
 using Shop.Domain;
-using Shop.Services;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json;
 using System.Linq;
 using System.Data.Common;
 using System.Data.SqlClient;
+using Shop.DataAccess;
 
 namespace Shop.UI
 {
@@ -23,46 +18,25 @@ namespace Shop.UI
             var providerName = configurationRoot.GetSection("AppConfig").GetChildren().Single(item => item.Key == "ProviderName").Value;
             var connectionString = configurationRoot.GetConnectionString("MyConnectionString");
 
-            DbProviderFactories.RegisterFactory(providerName, SqlClientFactory.Instance);
 
-            List<Category> categories = new List<Category>()
-            {
-                new Category()
-                {
-                Name = "Phone",
-                ImagePath = "C:/",
-                },
-                new Category()
-                {
-                Name = "Phone",
-                ImagePath = "C:/",
-                },
-            };
+
+
             Category category = new Category()
             {
-                Id = Guid.Parse("6C042E03-5AA9-4F66-902D-A3C183E89AE7"),
-                Name = "Gal",
-                ImagePath = "D:/",
+                Name = "fdgdfhfgh",
+                ImagePath = "C:/data",
             };
 
-            User user = new User()
+            using (var context = new ShopContext(connectionString))
             {
-                Email = "tnik8080@gmail.com",
-                Password = "DD123123",
-                Address = "Street Pushkin",
-                PhoneNumber = "845714115402",
-                VerificationCode = "123"
-            };
-            Repository<Category> repository = new Repository<Category>(connectionString);
-            //repository.Add(category);
-            //repository.SaveChanges();
-            //category.Name = "Galym";
-            //repository.SaveChanges();
-            //AuthService auth = new AuthService(connectionString);
-            //auth.SignUp("test@gmail.com","222");
-            //repository.Delete(Guid.Parse("6C042E03-5AA9-4F66-902D-A3C183E89AE7"));
-            //repository.SaveChanges();
-            repository.GetAll(category);
+                context.Categories.Add(category);
+                var result = context.Categories.ToList();
+                context.Categories.Remove(category);
+
+                context.Remove(category);
+                context.SaveChanges();
+            }
+
         }
     }
 }
